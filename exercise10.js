b6bd6d3f837887cde8ae3e96bae20605225d51e5
vinicius-hayden <vinihaydenss@gmail.com>
2022-03-29 04:32:34 -0300

    meter per second to kilometers per hour converter

--- exercise10.js
+++ exercise10.js
@@ -1,7 +1,17 @@
-// Create an algorithm that converts a speed given in m/s by the user to
+// Create an algorithm that converts a given speed in m/s by the user to
 // km/h. To do this, multiply the value in m/s by 3.6.
 
 function mstoKm(speed){
-    var kmperh = speed * 3.6
-    console.log(kmperh);
+    if(typeof(speed) == "number"){
+        var kmperh = speed * 3.6
+        console.log(kmperh);
+    }
+    else{
+        console.log("Invalid Input: make sure you type in a number.")
+    }
 }
+
+// if given speed parameter is not a number
+mstoKm("Hello!");
+// if given speed parameter is a number
+mstoKm(3.6);
