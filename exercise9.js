2a8f60604fd0a8cb5fa7bd8b3de7509d6f2b1864
vinicius-hayden <vinihaydenss@gmail.com>
2022-03-29 04:23:03 -0300

    arithmetic mean of a semester and bimester with two grades each

--- exercise9.js
+++ exercise9.js
@@ -2,23 +2,51 @@
 // by the arithmetic mean between the 2-month grade. Each quarter note is composed of
 // 2 exam notes.
 var gets = require('readline-sync');
-
+var i = 0;
 function arithmeticMean(num1, num2){
     let mean = (num1+num2)/2
     return mean;
 }
 
-function semesterGrade(){
-    var firstGradeFirstBimester =  gets.questionInt("Type your first grade of your first bimester: ")
-    var secondGradeFirstBimester = gets.questionInt("Type your second grade of your first bimester: ")
-    var firstBimesterMean = arithmeticMean(firstGradeFirstBimester,secondGradeFirstBimester)
-    console.log(`First bimester mean: ${firstBimesterMean}`);
+function questionDataType(number_data){
+    if(typeof(number_data) != "number"){
+        console.log("Invalid Input: please make sure to insert a number")
+        return false
+    }
+    else{
+        return true
+    }
+}
 
-    var firstGradeSecondBimester =  gets.questionInt("Type your first grade of your second bimester: ")
-    var secondGradeSecondBimester = gets.questionInt("Type your second grade of your second bimester: ")
-    var secondBimesterMean = arithmeticMean(firstGradeSecondBimester,secondGradeSecondBimester)
-    console.log(`Second bimester mean: ${secondBimesterMean}`);
-    console.log(`Semester mean: ${arithmeticMean(firstBimesterMean,secondBimesterMean)}`)
+function semesterGrade(){
+    while(i <= 0){
+        var firstGradeFirstBimester =  gets.questionInt("Type your first grade of your first bimester: ")
+        questionDataType(firstGradeFirstBimester);
+        if(questionDataType(firstGradeFirstBimester) == false){
+            break
+        }
+        var secondGradeFirstBimester = gets.questionInt("Type your second grade of your first bimester: ")
+        if(questionDataType(secondGradeFirstBimester) == false){
+            break
+        }
+        var firstBimesterMean = arithmeticMean(firstGradeFirstBimester,secondGradeFirstBimester)
+        console.log(`First bimester mean: ${firstBimesterMean}`);
+
+        var firstGradeSecondBimester =  gets.questionInt("Type your first grade of your second bimester: ")
+        questionDataType(firstGradeSecondBimester);
+        if(questionDataType(secondGradeFirstBimester) == false){
+            break
+        }
+        var secondGradeSecondBimester = gets.questionInt("Type your second grade of your second bimester: ")
+        questionDataType(secondGradeSecondBimester);
+        if(questionDataType(secondGradeFirstBimester) == false){
+            break
+        }
+        var secondBimesterMean = arithmeticMean(firstGradeSecondBimester,secondGradeSecondBimester)
+        console.log(`Second bimester mean: ${secondBimesterMean}`);
+        console.log(`Semester mean: ${arithmeticMean(firstBimesterMean,secondBimesterMean)}`)
+        i = i + 1;
+    }
 }
 
 semesterGrade()
