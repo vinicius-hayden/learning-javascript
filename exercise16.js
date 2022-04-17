cf9b5471b433c1c8dc20ca2c509cf7a4646660c3
vinicius-hayden <vinihaydenss@gmail.com>
2022-04-16 21:40:40 -0300

    positive or negative number function

---
+++ exercise16.js
@@ -0,0 +1,10 @@
+// Write a program that receives a number and print wheter it's positive or negative.
+
+function positiveOrNegative(number){
+  if(number > 0){
+    console.log('Positive')
+  }
+  else{
+    console.log('Negative')
+  }
+}
