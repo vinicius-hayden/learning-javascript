a1cadde9c48c5cf050519db31ba342fe5ec3dbb2
vinicius-hayden <vinihaydenss@gmail.com>
2022-04-17 23:15:49 -0300

    even or uneven number

---
+++ exercise18.js
@@ -0,0 +1,12 @@
+// Write a program that takes a number and prints whether this number is even or not.
+
+function evenNumber(num){
+  if(num%2 == 0){
+    console.log('even');
+  }
+  else{
+    console.log('uneven');
+  }
+}
+
+evenNumber(55);
