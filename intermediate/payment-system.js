7fdbb1abe70c27c2d279993c3739f747bd225edf
vinicius-hayden <vinihaydenss@gmail.com>
2022-04-23 23:47:45 -0300

    payment system function added

---
+++ intermediate/payment-system.js
@@ -0,0 +1,16 @@
+// A store sells its products in the input system plus two installments, the input being
+// greater than or equal to the two installments, which must be equal, whole and as large as possible.
+// For example, if the value of the goods is R$ 270.00, the down payment and the two installments are equal to R$ 90.00; 
+// if the value of the goods is BRL 302.75, the down payment is BRL 102.75 and the two installments are equal to BRL 100.00. 
+//Write a program that receives the value of the merchandise and provides the value of the entry and the two installments, 
+//according to the rules above. 
+// Note that a justification for the adoption of this rule is that it facilitates the preparation and consequent payment of 
+// the slips of the two installments.
+
+// Entrada pode ser maior ou igual às duas prestações.
+// As prestações tem que ser iguais umas às outras, maiores possíveis e INTEIRAS.
+
+function paymentSystem(priceProduct) {
+    let payment = priceProduct
+    return payment
+}
